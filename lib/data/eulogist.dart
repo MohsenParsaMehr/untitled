912078bcf2709ff27b3873bf45c47e2faaacd86d
Mohsen Parsamehr <parsamehr.email.box@gmail.com>
2023-08-26 13:56:26 +0330

    update

---
+++ lib/data/eulogist.dart
@@ -0,0 +1,17 @@
+class eulogist{
+  int id = 0;
+  String name;
+  String status;
+  DateTime? insertDate;
+  DateTime? updateDate;
+  String? description;
+  String? profilePhoto;
+  eulogist(
+      this.name,
+      this.status,
+      this.description,
+      this.profilePhoto
+
+      );
+
+}
---
+++ lib/data/eulogy.dart
@@ -0,0 +1,15 @@
+class eulogy{
+  int id = 0;
+  String kind;
+  String url;
+  DateTime? insertDate;
+  DateTime? updateDate;
+  String? description;
+  String? status;
+eulogy(
+    this.kind,
+    this.url,
+
+    );
+
+}
