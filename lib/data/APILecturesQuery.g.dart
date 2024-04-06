cd186bb67a1abfe15ae66cd68f9cf5aeb21754ec
parsa <parsa@SHS-MPARSAMEHR>
2024-04-06 14:17:21 +0330

    minor

--- lib/data/APILecturesQuery.g.dart
+++ lib/data/APILecturesQuery.g.dart
@@ -11,16 +11,26 @@ APILecturesQuery _$APILecturesQueryFromJson(Map<String, dynamic> json) =>
       id: json['id'] as int?,
       topic: json['topic'] as String,
       body: json['body'] as String,
-    )
-      ..status = json['status'] as String?
-      ..isBookmarked = json['isBookmarked'] as bool?
-      ..description = json['description'] as String?
-      ..locale = json['locale'] as String?
-      ..mediaUrl = json['mediaUrl'] as String?
-      ..forDate = json['forDate'] == null
+      lectureBody: json['lectureBody'] as String?,
+      type: json['type'] as String?,
+      author: json['author'] as String?,
+      captionImageUrl: json['captionImageUrl'] as String?,
+      description: json['description'] as String?,
+      locale: json['locale'] as String?,
+      status: json['status'] as String?,
+      isBookmarked: json['isBookmarked'] as bool?,
+      publishDate: json['publishDate'] == null
           ? null
-          : DateTime.parse(json['forDate'] as String)
-      ..operatorName = json['operatorName'] as String?;
+          : DateTime.parse(json['publishDate'] as String),
+      mediaUrl: json['mediaUrl'] as String?,
+      operatorName: json['operatorName'] as String?,
+      insertDate: json['insertDate'] == null
+          ? null
+          : DateTime.parse(json['insertDate'] as String),
+      updateDate: json['updateDate'] == null
+          ? null
+          : DateTime.parse(json['updateDate'] as String),
+    );
 
 Map<String, dynamic> _$APILecturesQueryToJson(APILecturesQuery instance) =>
     <String, dynamic>{
@@ -32,6 +42,12 @@ Map<String, dynamic> _$APILecturesQueryToJson(APILecturesQuery instance) =>
       'description': instance.description,
       'locale': instance.locale,
       'mediaUrl': instance.mediaUrl,
-      'forDate': instance.forDate?.toIso8601String(),
+      'publishDate': instance.publishDate?.toIso8601String(),
+      'lectureBody': instance.lectureBody,
+      'type': instance.type,
+      'author': instance.author,
+      'captionImageUrl': instance.captionImageUrl,
+      'insertDate': instance.insertDate?.toIso8601String(),
+      'updateDate': instance.updateDate?.toIso8601String(),
       'operatorName': instance.operatorName,
     };
